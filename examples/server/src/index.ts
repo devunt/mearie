abaecbdc3e974ddf199d5d83a15b80fa098c6f07
Bae Junehyeon <devunt@gmail.com>
2025-10-26 18:15:41 +0900

    feat(fixture): use realistic reviews

--- examples/server/src/index.ts
+++ examples/server/src/index.ts
@@ -1,11 +1,20 @@
 import { createYoga } from 'graphql-yoga';
-import { schema } from '@mearie-internal/fixture';
+import { schema, seedReviews } from '@mearie-internal/fixture';
 
 const yoga = createYoga({
   schema,
   landingPage: false,
 });
 
+let initialized = false;
+
 export default {
-  fetch: yoga,
+  fetch(request: Request) {
+    if (!initialized) {
+      seedReviews();
+      initialized = true;
+    }
+
+    return yoga.fetch(request);
+  },
 };
--- fixture/src/index.ts
+++ fixture/src/index.ts
@@ -2,3 +2,4 @@ export { default as movies } from '../data/movies.json';
 export { default as people } from '../data/people.json';
 export { default as genres } from '../data/genres.json';
 export { schema } from './executable-schema.ts';
+export { seedReviews } from './resolvers.ts';

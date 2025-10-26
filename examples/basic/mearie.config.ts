fa17fecd714dde75f302b5fad65d3e1b5d8dd6be
Bae Junehyeon <devunt@gmail.com>
2025-10-27 03:10:41 +0900

    chore(examples): remove basic example in favor of react example

--- examples/basic/mearie.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'mearie';
-
-export default defineConfig({
-  scalars: {
-    DateTime: 'Date',
-    JSON: 'Record<string, unknown>',
-  },
-});
--- examples/basic/vite.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite';
-import mearie from 'mearie/vite';
-
-export default defineConfig({
-  plugins: [mearie()],
-});
